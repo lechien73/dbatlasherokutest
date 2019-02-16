9bf062e951cc3fdfd8a9bec67f4881e1a7aafd85
Matt Rudge <matt.rudge@codeinstitute.net>
2019-02-16 16:58:16 +0000

    Fixed app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from bson.objectid import ObjectId
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'task_manager'
-MONGO_URI = os.getenv("MONGO_URI")
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 
 mongo = PyMongo(app)
 
